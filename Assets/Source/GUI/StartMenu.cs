12b4b344c6218f5ee4269bf6cb3813c366adfcc1
trokair <wandos1@yahoo.com>
2017-01-21 15:58:24 -0600

    Added StartMenu
    Added the start menu scene with music, background, and gui script.

---
+++ Assets/Source/GUI/StartMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SeasideScroll.GUI
+{
+	public class StartMenu : MonoBehaviour 
+	{
+		[SerializeField]
+		private string _startScene;
+
+		public void StartGame ()
+		{
+			SceneManager.LoadScene (_startScene);
+		}
+	}
+}
