b6344ffb8e5ea5a3983a6fdebec191078dda7fec
Christopher Taylor <christopher.taylor@rtegaming.com>
2017-01-22 10:57:02 -0600

    added actual level art into game

--- Assets/Source/TriggerSystem.cs
+++ Assets/Source/TriggerSystem.cs
@@ -6,10 +6,16 @@ namespace SeaSideScroll
     public class TriggerSystem : MonoBehaviour
     {
         public UnityEvent OnEnter;
+        [SerializeField]
+        private LayerMask _mask;
 
         private void OnTriggerEnter2D(Collider2D collider)
         {
-            OnEnter.Invoke();
+            if (_mask == (_mask | (1 << collider.gameObject.layer)))
+            {
+                OnEnter.Invoke();
+            }
+            
         }
     }
 }
